beacc1ed95cb9ec557c1048d946a2309d8086709
Michael Munje <michaelmunje@gmail.com>
2019-01-12 12:27:07 -0800

    Fixed string input to work in python 2.7

--- nltk/test.py
+++ nltk/test.py
@@ -2,7 +2,7 @@ import nltk
 from nltk.corpus import cmudict 
 d = cmudict.dict() 
 
-sentence = input("Enter a sentence: ")
+sentence = raw_input("Enter a sentence: ")
 sentence = sentence.lower()
 words = sentence.split(' ')
 for word in words:
